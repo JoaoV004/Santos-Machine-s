52e15240c183e43c3c0d90bb3724a77cbc0952aa
João Vitor Oliveira da Silva <joao.osilva@sptech.school>
2024-03-30 23:42:07 -0300

    Subindo atualizações no front end

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.title', { duration: 1500});
+sr.reveal('.title', { 
+    rotate: {x:100, y:100, z:0,},
+    duration: 1000});
 
 sr.reveal('.decimal', { duration: 3000});
 function converterDecimal() {
